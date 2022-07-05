6f60ba22569c2d35673f17a317e799c5aafea3c3
王自权 <wangziquan@hetao101.com>
2022-07-05 11:04:35 +0800

    UIBase组件绑定跟节点绑定

--- assets/script/ui/UIBase.ts
+++ assets/script/ui/UIBase.ts
@@ -40,9 +40,9 @@ export default class UIBase extends cc.Component {
 
     protected lateLoad() {
         let start_time = new Date().getTime();
-        this.load_all_object(this.node, "");
+        this.load_all_object(this.node);
         UIBase.load_all_objects_duration += new Date().getTime() - start_time;
-        cc.log("load_all_object 总消耗时间(s):", new Date().getTime(),UIBase.load_all_objects_duration);
+        cc.log("load_all_object 总消耗时间(s):", new Date().getTime(), UIBase.load_all_objects_duration);
     }
 
     protected lateClose(param: any = null) {
@@ -70,17 +70,33 @@ export default class UIBase extends cc.Component {
     }
 
     /**
-     * 保证节点名字在根节点下唯一性
+     * 保证节点名字在根节点下唯一性 最好不要取名view
      * @param root 
      * @param path 
+     * @param 获取节点下的组件的方式 root.$Sprite--->root.$Button---直接获取
      */
-    private load_all_object(root: cc.Node, path: string) {
+    load_all_object(root: cc.Node): void {
         for (let i = 0; i < root.childrenCount; i++) {
-            this.view[root.children[i].name] = root.children[i];
-            this.load_all_object(root.children[i], path + root.children[i].name + "/");
+            let child: cc.Node = root.children[i];
+            console.log("child", child);
+            //@ts-ignore
+            child._components.forEach(nodeComponent => {
+                let name = this._getComponentName(nodeComponent);
+                if (name !== "Widget") {
+                    name = `$${name}`;
+                    child[name] = nodeComponent;
+                }
+            });
+            this.view[root.children[i].name] = child;
+            this.load_all_object(root.children[i]);
         }
     }
-
-
+    /**
+     * 获取组件名字
+     * @param {cc.Component} component 
+     */
+    _getComponentName(component: any): string {
+        return component.name.match(/<.*>$/)[0].slice(1, -1);
+    }
 
 }
