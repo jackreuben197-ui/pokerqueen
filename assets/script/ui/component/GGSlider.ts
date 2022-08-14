6243c4411881b1253cccce1fdc2f6e39f1ecfdfe
yechun <21882889@qq.com>
2022-08-14 23:54:37 +0800

    滑动条组件

---
+++ assets/script/ui/component/GGSlider.ts
@@ -0,0 +1,98 @@
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class GGSlider extends cc.Component {
+
+    @property(cc.Node)
+    track_back: cc.Node = null;
+
+    @property(cc.Node)
+    track_top: cc.Node = null;
+
+    @property(cc.Node)
+    bar: cc.Node = null;
+
+    @property({ range: [0, 50], step: 1 })
+    min: number = 1;
+    @property({ range: [0, 50], step: 1 })
+    max: number = 3;
+
+    step_count: number;
+
+    press: boolean;
+
+    step_dis: number;
+
+    private _pos: number;
+    private _param: any;
+
+    onLoad() {
+        this.step_count = this.max - this.min;
+        this.step_dis = this.node.width / this.step_count;
+        this.bar.on(cc.Node.EventType.TOUCH_START, this.onBarTouchStart, this);
+        this.bar.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
+        this.bar.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.bar.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+
+        this.track_back.on(cc.Node.EventType.TOUCH_START, this.onTrackTouchStart, this);
+        this.track_back.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
+        this.track_back.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.track_back.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+
+        this.reset();
+    }
+
+
+    reset(): void {
+        this.setBarPos(this.min);
+    }
+
+    onShow(param?: any) {
+        this._param = param;
+        this.setBarPos(param?.pos || this.min);
+    }
+
+    onBarTouchStart(e: cc.Event.EventTouch) {
+        this.press = true;
+    }
+    onTouchMove(e: cc.Event.EventTouch) {
+        if (this.press) {
+            let w_location = e.getLocation();
+            let l_location = this.node.convertToNodeSpaceAR(w_location);
+            let x = l_location.x;
+            if (l_location.x < 0) x = Math.max(l_location.x, 0);
+            if (l_location.x > this.node.width) x = Math.min(l_location.x, this.node.width);
+            this.setBarPos(this.min + this.postion2pos(x));
+        }
+    }
+    onTouchEnd() {
+        this.press = false;
+    }
+
+    onTrackTouchStart(e: cc.Event.EventTouch) {
+        this.press = true;
+        let w_location = e.getLocation();
+        let l_location = this.node.convertToNodeSpaceAR(w_location);
+        this.setBarPos(this.min + this.postion2pos(l_location.x));
+    }
+
+    postion2pos(postion: number) {
+        return Math.ceil((postion - this.step_dis / 2) / this.step_dis);
+    }
+
+    setBarPos(pos: number) {
+        this.pos = pos;
+        let step = pos - this.min;
+        this.bar.x = step / this.step_count * this.node.width;
+        this.track_top.width = this.bar.x;
+        //响应回调
+        this._param?.onChange(this);
+    }
+    get pos(): number {
+        return this._pos;
+    }
+    set pos(pos: number) {
+        this._pos = pos;
+    }
+}
