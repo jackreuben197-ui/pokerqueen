04dd81b5ea5307078ed63c4374f4b427daa64dac
yechun <21882889@qq.com>
2022-09-23 17:41:17 +0800

    加入GM加钱命令

--- assets/script/gm/UIGMComponent.ts
+++ assets/script/gm/UIGMComponent.ts
@@ -1,23 +1,76 @@
 
 
+import { json } from "stream/consumers";
 import { GameCache } from "../game/GameCache";
 import UIBase from "../ui/UIBase";
-import { GM_Templete } from "./GMAPI";
+import UIComponent from "../ui/UIComponent";
+import { GM, GM_Templete, Web_GMC_Recharge } from "./GMAPI";
+
+
+export enum GM_CMD {
+    AddCoin,
+
+}
+export var CMD_Call = {
+    [GM_CMD.AddCoin]: GM.Web_GMC_Recharge,
+}
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class UIGMComponent extends UIBase {
 
+    submit_text: cc.EditBox = null;
 
+    currCMD: GM_CMD;
+
+    lateLoad() {
+        super.lateLoad();
+        this.submit_text = this.getChildNodeOrComponent("submit_text", cc.EditBox);
+    }
+
+    async onSubmit() {
+        let trim_text = this.trim(this.submit_text.string);
+        let param = null;
+        try {
+            param = JSON.parse(trim_text);
+        } catch (e) {
+            UIComponent.Instance.Toast("指令参数错误");
+        }
+        if (param) {
+            let result = await CMD_Call[this.currCMD](param);
+            if (result) {
+                UIComponent.Instance.Toast(`${GM_CMD[this.currCMD]} 成功`);
+            }
+        }
+    }
 
     //增加金币
     onAddCoin() {
         let param = [GameCache.Instance.nUserId];
 
-        //for()
+        let text: string = GM_Templete.Recharge;
 
-        let text = GM_Templete.Recharge.replace
+        for (let i = 0; i < param.length; i++) {
+            text = text.replace(`%${i}`, `${param[i]}`);
+        }
+
+        this.submit_text.string = text;
+
+        this.currCMD = GM_CMD.AddCoin;
+
+        // let trim_text = text.trim().replace(/\n/g, "").replace(/ /g, "");
+
+        // console.log(trim_text);
+
+        // let json = JSON.parse(trim_text);
+
+        // console.log(json);
+
+    }
+
+    trim(text: string): string {
+        return text.trim().replace(/\n/g, "").replace(/ /g, "");
     }
 
 }
