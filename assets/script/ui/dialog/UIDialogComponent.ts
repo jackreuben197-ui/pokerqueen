c53b1fd0a5f415ff4a7f60e02d9b7fa1a1ae957f
xuyanhe <1536337341@qq.com>
2022-09-27 19:00:28 +0800

    提示框修改

--- assets/script/ui/dialog/UIDialogComponent.ts
+++ assets/script/ui/dialog/UIDialogComponent.ts
@@ -110,13 +110,10 @@ export default class UIDialogComponent extends BaseTouchBoard {
 
         this.Button_Cancel.active = param?.type == UIDialogComponent.DialogType.CommitCancel;
 
-        this.Text_Title.string = param?.title || "";
-
-        this.Text_Content.string = param?.content || "";
-
-        this.Text_Commit.string = param?.contentCommit || "ok";
-
-        this.Text_Cancel.string = param?.contentCancel || "cancel";
+        this.setText(this.Text_Title, param?.title || "")
+        this.setText(this.Text_Content, param?.content || "")
+        this.setText(this.Text_Commit, param?.contentCommit || "ok")
+        this.setText(this.Text_Cancel, param?.contentCancel || "cancel")
 
         this._actionCommit = param?.actionCommit || null;
         this._actionCancel = param?.actionCancel || null;
