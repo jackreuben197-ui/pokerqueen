231f1ad91e81d1f8c95986318830be137951905e
xuefujin <xuefujinxfj0703@163.com>
2022-11-21 16:08:58 +0800

    增加code码

--- assets/script/config/GameConfig.ts
+++ assets/script/config/GameConfig.ts
@@ -3,7 +3,7 @@
  * @Date: 2022-09-19 17:20:26
  * @description: 
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-11-14 12:38:29
+ * @LastEditTime: 2022-11-21 15:31:42
  * @FilePath: /pokerqueen/assets/script/config/GameConfig.ts
  */
 /**
@@ -23,7 +23,7 @@ export class GameConfig {
     //2: http://dev1.awanptesting.com 
     //3: https://test1.awanptesting.com  
     //4: https://dev1.awanptesting.com 
-    static BuildType: number = 3;
+    static BuildType: number = 4;
     //版本号
     static Version: string = "20221117_1500";
 
