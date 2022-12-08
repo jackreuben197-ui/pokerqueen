168ca421c4e58f42f292299a119411cfc0303fa7
yechun <21882889@qq.com>
2022-12-08 17:15:59 +0800

    修复ui走查bug

--- assets/script/config/GameConfig.ts
+++ assets/script/config/GameConfig.ts
@@ -28,7 +28,7 @@ export class GameConfig {
     //4: https://dev1.awanptest.com 
     static BuildType: number = 3;
     //版本号
-    static Version: string = "20221208_1700";
+    static Version: string = "20221208_1730";
 
     static Default_Language: string = "cn";
     //是否使用代理
