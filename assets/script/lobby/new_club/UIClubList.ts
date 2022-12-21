1160aa08e5ed7dc0dc2d221e2675a64df4297b8f
xuefujin <xuefujinxfj0703@163.com>
2022-12-21 15:48:11 +0800

    add

--- assets/script/lobby/new_club/UIClubList.ts
+++ assets/script/lobby/new_club/UIClubList.ts
@@ -3,12 +3,18 @@
  * @Date: 2022-12-21 11:16:27
  * @description: 
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-12-21 12:12:37
+ * @LastEditTime: 2022-12-21 15:47:11
  * @FilePath: /pokerqueen/assets/script/lobby/new_club/UIClubList.ts
  */
 
-import { Web_Org_Club_Get } from "../../net/https/WebRequest";
+import { UIDefine } from "../../define/UIDefine";
+import { GameCache } from "../../game/GameCache";
+import { StringHelper } from "../../helper/StringHelper";
+import WebImageHelper from "../../helper/WebImageHelper";
+import SceneManager from "../../manager/SceneManager";
+import { Web_Org_Club_Get, Web_User_Info } from "../../net/https/WebRequest";
 import BaseForm from "../../ui/form/BaseForm";
+import UIComponent from "../../ui/UIComponent";
 import { UIClubModel } from "../labor/UIClubModel";
 
 const { ccclass, property, menu } = cc._decorator;
@@ -17,12 +23,18 @@ const { ccclass, property, menu } = cc._decorator;
 export default class UIClubList extends BaseForm {
     @property(cc.Prefab)
     clubListItem: cc.Prefab = null;
+    @property(cc.Prefab)
+    clubPageItem: cc.Prefab = null;
+
+    @property(cc.PageView)
+    pageViews: cc.PageView = null;
+
     @property(cc.Label)
     num: cc.Label = null;
     topNode: cc.Node
     pageNode: cc.Node
     listNode: cc.Node
-    listType = 1;
+    listType = 2;
     protected lateLoad(): void {
         super.lateLoad();
         this.topNode = this.getChildNodeOrComponent("topNode");
@@ -37,6 +49,15 @@ export default class UIClubList extends BaseForm {
         let data: any = Web_Org_Club_Get.Response.data
         this.num.string = data.length;
         this.initListNode(data)
+        this.initPageNode(data)
+        this.initTop()
+    }
+    initTop() {
+        let icon = cc.find('iconMask/icon', this.topNode).getComponent(cc.Sprite);
+        WebImageHelper.SetHeadImage(icon, GameCache.Instance.headPic)
+        this.topNode.getChildByName('name').getComponent(cc.Label).string = StringHelper.LengthNick(Web_User_Info.Response.data.user.nickname);
+        this.topNode.getChildByName('id').getComponent(cc.Label).string = Web_User_Info.Response.data.user.un_id + ""
+
     }
     initListNode(clubList) {
         let menberlist = this.listNode.getChildByName('menberlist');
@@ -48,6 +69,39 @@ export default class UIClubList extends BaseForm {
             clubListItem.parent = sv_content;
             clubListItem.getComponent('clubListItem').initData(element);
         }
+    }
+    initPageNode(clubList) {
+        let menberlist = this.pageNode.getChildByName('PageView');
+        let content = cc.find('view/content', menberlist);
+        content.removeAllChildren();
+        for (let index = 0; index < clubList.length; index++) {
+            const element = clubList[index];
+            let clubPageItem = cc.instantiate(this.clubPageItem);
+            this.pageViews.addPage(clubPageItem)
+            clubPageItem.getComponent('clubPageItem').initData(element);
+        }
+        let clubPageItem = cc.instantiate(this.clubPageItem);
+        this.pageViews.addPage(clubPageItem)
+        clubPageItem.getComponent('clubPageItem').initData(null);
+
+    }
+    ceateClub() {
+        UIComponent.open(UIDefine.UICreatelabor, null, { SceneUI: SceneManager.Instance.currUI });
+    }
+    joinClub() {
+        UIComponent.open(UIDefine.UIlaborJoin, null, { SceneUI: SceneManager.Instance.currUI });
+    }
+    changeMenu() {
+
+        let union = cc.find('menuChange/union', this.topNode);
+        let list = cc.find('menuChange/list', this.topNode);
+
+        this.listType = this.listType == 1 ? 2 : 1
+        this.pageNode.active = this.listType == 2
+        this.listNode.active = this.listType == 1
+        union.active = this.pageNode.active
+        list.active = this.listNode.active
+
     }
 
 }
---
+++ assets/script/lobby/new_club/clubPageItem.ts
@@ -0,0 +1,66 @@
+/*
+ * @Author: xfj
+ * @Date: 2022-10-28 17:58:45
+ * @description: 
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-12-21 15:22:14
+ * @FilePath: /pokerqueen/assets/script/lobby/new_club/clubPageItem.ts
+ */
+
+import { UIDefine } from "../../define/UIDefine";
+import { ClubCache } from "../../frame/data/club/ClubCache";
+import TimeHelper from "../../helper/TimeHelper";
+import WebImageHelper from "../../helper/WebImageHelper";
+import SceneManager from "../../manager/SceneManager";
+import UIBase from "../../ui/UIBase";
+import UIComponent from "../../ui/UIComponent";
+
+const { ccclass, property, menu } = cc._decorator;
+
+@ccclass
+
+@menu('脚本分组/new_club/clubPageItem')
+
+export default class clubPageItem extends UIBase {
+    @property(cc.Node)
+    messNode: cc.Node = null;
+
+    @property(cc.Node)
+    nomalNode: cc.Node = null;
+    _data = null;
+    initData(data) {
+        this._data = data
+        this.messNode.active = this._data != null
+        this.nomalNode.active = this._data == null
+        if (this._data) {
+            this.initView();
+        }
+    }
+    async initView() {
+
+        cc.find('messLayout/name', this.messNode).getComponent(cc.Label).string = this._data.club_name
+        cc.find('messLayout/id', this.messNode).getComponent(cc.Label).string = 'ID:' + this._data.random_id
+        cc.find('table/data', this.messNode).getComponent(cc.Label).string = this._data.tables
+        cc.find('people/data', this.messNode).getComponent(cc.Label).string = this._data.club_members
+        let icon = cc.find('Group_3340/iconMask/icon', this.messNode);
+        // WebImageHelper.setImageSize(icon.getComponent(cc.Sprite), 398, 398)
+
+        WebImageHelper.SetHeadImage(icon.getComponent(cc.Sprite), this._data.logo)
+        this.node['info'] = this._data;
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onClickItem, this)
+        let iconRole = cc.find('iconRole/icon', this.messNode);
+    }
+    initView1() {
+
+    }
+
+    onClickItem(event) {
+        let target = event.target;
+        let info = target.info;
+        ClubCache.setClubData(info);
+        UIComponent.open(UIDefine.UIClubHome, info, { SceneUI: SceneManager.Instance.currUI });
+    }
+    createClub() {
+        UIComponent.open(UIDefine.UICreatelabor, null, { SceneUI: SceneManager.Instance.currUI });
+    }
+}
