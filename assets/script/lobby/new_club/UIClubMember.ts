9858d1f2a7deeb6e67a1606ea33cf2c8b9a173ef
xuefujin <xuefujinxfj0703@163.com>
2022-12-22 18:44:05 +0800

    add

--- assets/script/lobby/new_club/UIClubMember.ts
+++ assets/script/lobby/new_club/UIClubMember.ts
@@ -3,7 +3,7 @@
  * @Date: 2022-12-22 13:13:05
  * @description: 
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-12-22 17:30:50
+ * @LastEditTime: 2022-12-22 17:51:45
  * @FilePath: /pokerqueen/assets/script/lobby/new_club/UIClubMember.ts
  */
 // Learn TypeScript:
