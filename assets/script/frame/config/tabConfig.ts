1b9c69f7c6a4f690ef9ea1112b5488a726a83478
xuefujin <xuefujinxfj0703@163.com>
2023-01-13 13:02:05 +0800

    。。。

--- assets/script/frame/config/tabConfig.ts
+++ assets/script/frame/config/tabConfig.ts
@@ -3,9 +3,10 @@
  * @Date: 2023-01-13 12:26:50
  * @description: 
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-01-13 12:42:41
+ * @LastEditTime: 2023-01-13 13:01:49
  * @FilePath: /pokerqueen/assets/script/frame/config/tabConfig.ts
  */
+// data  读多语言   默认值可以不填
 export const FriendMathTabConfig = { data: ['UIClub_Match', 'adaptation10124'], defaultIndex: 0, defaultWidth: 977, defaultHeight: 159, defaultFontSize: 46 }
 
 
