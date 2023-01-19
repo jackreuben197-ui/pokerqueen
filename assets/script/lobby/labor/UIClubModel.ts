4feda846ff57982deaa3f6b2d92595acaf9c0ff3
yechun <21882889@qq.com>
2023-01-19 22:37:02 +0800

    fix

--- assets/script/lobby/labor/UIClubModel.ts
+++ assets/script/lobby/labor/UIClubModel.ts
@@ -9,7 +9,7 @@
 
 import { ClubCache } from "../../frame/data/club/ClubCache";
 import HttpRequest from "../../net/https/HttpRequest";
-import { APIOrgClubAgentUser_list, APIOrgClubShareApproveList, APIOrgClubShareApplyList, APIOrgClubShareAudit, APIOrgClubSharePendingList, APIOrgClubNotice, APIOrgClubNotice_update, APIOrgRoomClubCreate, APIOrgRoomBatchCreate, APIOrgClubCancleJoinTribe, APIOrgClubApplyTribeList, APIOrgClubUserRole_change, APIOrgClubUserGameInfo, APIOrgClubUserRemarks, APIOrgClubUserInfo, APIOrgGetNewMessNum, APIOrgGetMessList, APIOrgSendMess, APIOrgClubRoom, APIOrgClubUpLevel, APIOrgClubLevelCost, APIOrgClubLevelInfo, APIOrgClubLevelBenefit, APIOrgClubMemberEarning, APIOrgClubEarning, APIOrgClubActivityInfo, APIOrgClubActivityCreate, APIOrgClubDelAdmin, APIOrgClubCreateRoomChange, APIOrgClubMember, APIOrgClubAddAdmin, APIOrgFriendRoomInfo, APIOrgFriendApplyDeal, APIOrgFriendApplyList, APIOrgFriendRoomList, APIOrgInvitationRoom, APIOrgRoomConfigCreate, APIOrgGetRoomConfig, APIOrgRoomCreate, APIOrgUpdateTemplate, APIOrgTemplateDelete, APIOrgGetTemplate, APIOrgCreateTemplate, APIOrgChangeClubData, APIOrgJoinTrip, APIOrgTribeSearchByID, APIOrgClubGold, APIOrgMemberList, APIOrgMangerList, Web_Org_Club_Create, Web_Org_Club_Get, Web_Org_Club_Player_Apply_List, Web_Org_Club_Search_By_Id, Web_Org_Club_Join, APIOrgClubCancleJoinClub, APIOrgClubIsManger, APIOrgClubGetJoinlList, APIOrgClubApprovalJoin, APIOrgClubQuit, APIOrgClubUploadIcon, Web_Club_Fund_ChangeLog, Web_Recharge_Gold_Club, Web_Tiqu_Gold_Club, Web_Club_Fund_OrderList, Web_Club_Fund_ApplyList, Web_Club_Player_Order_Record } from "../../net/https/WebRequest";
+import { APIOrgClubShareApproveList, APIOrgClubShareApplyList, APIOrgClubShareAudit, APIOrgClubSharePendingList, APIOrgClubNotice, APIOrgClubNotice_update, APIOrgRoomClubCreate, APIOrgRoomBatchCreate, APIOrgClubCancleJoinTribe, APIOrgClubApplyTribeList, APIOrgClubUserRole_change, APIOrgClubUserGameInfo, APIOrgClubUserRemarks, APIOrgClubUserInfo, APIOrgGetNewMessNum, APIOrgGetMessList, APIOrgSendMess, APIOrgClubRoom, APIOrgClubUpLevel, APIOrgClubLevelCost, APIOrgClubLevelInfo, APIOrgClubLevelBenefit, APIOrgClubMemberEarning, APIOrgClubEarning, APIOrgClubActivityInfo, APIOrgClubActivityCreate, APIOrgClubDelAdmin, APIOrgClubCreateRoomChange, APIOrgClubMember, APIOrgClubAddAdmin, APIOrgFriendRoomInfo, APIOrgFriendApplyDeal, APIOrgFriendApplyList, APIOrgFriendRoomList, APIOrgInvitationRoom, APIOrgRoomConfigCreate, APIOrgGetRoomConfig, APIOrgRoomCreate, APIOrgUpdateTemplate, APIOrgTemplateDelete, APIOrgGetTemplate, APIOrgCreateTemplate, APIOrgChangeClubData, APIOrgJoinTrip, APIOrgTribeSearchByID, APIOrgClubGold, APIOrgMemberList, APIOrgMangerList, Web_Org_Club_Create, Web_Org_Club_Get, Web_Org_Club_Player_Apply_List, Web_Org_Club_Search_By_Id, Web_Org_Club_Join, APIOrgClubCancleJoinClub, APIOrgClubIsManger, APIOrgClubGetJoinlList, APIOrgClubApprovalJoin, APIOrgClubQuit, APIOrgClubUploadIcon, Web_Club_Fund_ChangeLog, Web_Recharge_Gold_Club, Web_Tiqu_Gold_Club, Web_Club_Fund_OrderList, Web_Club_Fund_ApplyList, Web_Club_Player_Order_Record } from "../../net/https/WebRequest";
 import upLoadIcon from "../upLoadIcon";
 
 export class UIClubModel {
@@ -1094,22 +1094,6 @@ export class UIClubModel {
             });
         });
     }
-    //公会代理下线成员列表
-    APIOrgClubAgentUser_list(param) {
-        return new Promise((resolve, reject) => {
-            HttpRequest.Send({
-                request: APIOrgClubAgentUser_list,
-                body: APIOrgClubAgentUser_list.Request(param),
-                onSuccess: function () {
-                    resolve(APIOrgClubAgentUser_list.Response);
-                }.bind(this),
-                onFailure: function (content) {
-                    reject(content);
-                }.bind(this),
-            });
-        });
-    }
-
     //////////////////////////////////////////////////////
-    
+
 }
