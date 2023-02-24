8afca2c00d5600b820555c5258a664cf57f07bdb
xuefujin <xuefujinxfj0703@163.com>
2023-02-24 16:07:08 +0800

    add  汇率

--- assets/script/frame/config/tabConfig.ts
+++ assets/script/frame/config/tabConfig.ts
@@ -3,7 +3,7 @@
  * @Date: 2023-01-13 12:26:50
  * @description: 
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-02-23 20:19:24
+ * @LastEditTime: 2023-02-24 15:53:50
  * @FilePath: /pokerqueen/assets/script/frame/config/tabConfig.ts
  */
 // data  读多语言   默认值可以不填
@@ -12,5 +12,6 @@ export const CareerRecordTabConfig = { data: ['UIData_Today', 'UIData_Week', 'UI
 export const ClubTabConfig = { data: ['UIGuild_Tiny', 'UIGuild_Small', 'UIGuild_Middle', 'UIGuild_Big'], defaultIndex: 0, defaultWidth: 1110, defaultHeight: 159, defaultFontSize: 46 }
 export const joinClubConfig = { data: ['UISearchClub', 'UIApplyHistory'], defaultIndex: 0, defaultWidth: 1110, defaultHeight: 159, defaultFontSize: 46 }
 export const joinUnionConfig = { data: ['UISearchUnion', 'UIApplyHistory'], defaultIndex: 0, defaultWidth: 1110, defaultHeight: 159, defaultFontSize: 46 }
+export const rateTabConfig = { data: ['UIGuild_ExchangeRateSettingsGold', 'UIGuild_ExchangeRateSettingsUsdt'], defaultIndex: 0, defaultWidth: 1110, defaultHeight: 159, defaultFontSize: 46 }
 
 
