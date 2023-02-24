38cab2c945cd5b08e130e769658bd712afe16840
xuefujin <xuefujinxfj0703@163.com>
2023-02-24 16:51:47 +0800

    。。

--- assets/script/lobby/new_club/rateSet/UIClubRateSet.ts
+++ assets/script/lobby/new_club/rateSet/UIClubRateSet.ts
@@ -3,12 +3,17 @@
  * @Date: 2022-12-28 16:52:38
  * @description: 
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-02-02 11:44:41
+ * @LastEditTime: 2023-02-24 16:51:04
  * @FilePath: /pokerqueen/assets/script/lobby/new_club/rateSet/UIClubRateSet.ts
  */
 import ComFormTitle from "../../../common/ComFormTitle";
+import TabNode from "../../../common/tabNode";
+import { rateTabConfig } from "../../../frame/config/tabConfig";
 import { ClubCache } from "../../../frame/data/club/ClubCache";
+import { i18nMgr } from "../../../i18n/i18nMgr";
+import ToastManager from "../../../manager/ToastManager";
 import BaseForm from "../../../ui/form/BaseForm";
+import Toast from "../../../ui/toast/Toast";
 import UIComponent from "../../../ui/UIComponent";
 import { UIClubModel } from "../../labor/UIClubModel";
 const { ccclass, property, menu } = cc._decorator;
@@ -20,74 +25,45 @@ export default class UIClubRateSet extends BaseForm {
     private comFormTitle: ComFormTitle = null;
 
     @property(cc.EditBox)
-    goldEditBox: cc.EditBox = null;
+    editBox: cc.EditBox = null;
 
-    @property(cc.EditBox)
-    usdtEditBox: cc.EditBox = null;
-    @property(cc.Label)
-    lbl_usdt: cc.Label = null;
-    @property(cc.Label)
-    lbl_gold: cc.Label = null;
-
-    memberListT: cc.Node
-    applyListT: cc.Node
-    changeGold: cc.Node
-    changeUsdt: cc.Node
-    saveBtn: cc.Button
-
-    _selectTitle
+    upNode: cc.Node = null;
+    downNode: cc.Node = null;
+    tabNode: TabNode = null;
+    selectIndex = 0;
+    lbl_tip: cc.Label = null;
 
     protected lateLoad(): void {
         super.lateLoad();
-        this.memberListT = this.getChildNodeOrComponent("memberListT");
-        this.applyListT = this.getChildNodeOrComponent("applyListT");
         this.comFormTitle = this.getChildNodeOrComponent("comFormTitle", ComFormTitle);
-        this.changeGold = this.getChildNodeOrComponent("changeGold");
-        this.changeUsdt = this.getChildNodeOrComponent("changeUsdt");
-        this.saveBtn = this.getChildNodeOrComponent('saveBtn', cc.Button)
+        this.tabNode = this.getChildNodeOrComponent("tabNode", TabNode);
+        this.upNode = this.getChildNodeOrComponent("upNode");
+        this.downNode = this.getChildNodeOrComponent("downNode");
+        this.lbl_tip = this.getChildNodeOrComponent("lbl_tip", cc.Label);
     }
     async onShow(param?: any, fromUI?: cc.Node, sceneUI?: cc.Node) {
         super.onShow(param, fromUI, sceneUI);
-        let title = "UITitle_RateSet"
+        this.tabNode.initData(rateTabConfig, this.titleNodeClick.bind(this), this)
+        let title = "UIGuild_ExchangeRateSettings"
         this.comFormTitle.initData(title, this);
-        this.titleNodeClick(null, 0)
-        this.usdtEditBox.string = ClubCache.gold_to_usdt_rate
-        this.lbl_gold.string = this.goldEditBox.string + ' 金豆'
-        this.goldEditBox.string = ClubCache.usdt_to_gold_rate
-        this.lbl_usdt.string = this.usdtEditBox.string + ' USDT'
+        // this.titleNodeClick(0);
 
     }
-    titleNodeClick(event, customData) {
-        // if (this._selectTitle == customData) return
-        this._selectTitle = customData
-        this.memberListT.getChildByName('block').active = this._selectTitle == 0
-        this.applyListT.getChildByName('block').active = this._selectTitle == 1
+    titleNodeClick(customData) {
+        this.selectIndex = customData
+        this.upNode.getChildByName('coin01').active = customData == 0
+        this.upNode.getChildByName('coin02').active = customData == 1
 
-        this.memberListT.getChildByName('title').color = this._selectTitle == 0 ? cc.color().fromHEX('#35A3B3') : cc.color().fromHEX('#FFFFFF')
-        this.applyListT.getChildByName('title').color = this._selectTitle == 1 ? cc.color().fromHEX('#35A3B3') : cc.color().fromHEX('#FFFFFF')
-        this.changeGold.active = this._selectTitle == 0
-        this.changeUsdt.active = this._selectTitle == 1
-
+        this.downNode.getChildByName('coin01').active = customData == 1
+        this.downNode.getChildByName('coin02').active = customData == 0
+        this.editBox.string = this.selectIndex == 0 ? ClubCache.gold_to_usdt_rate : ClubCache.usdt_to_gold_rate
+        this.lbl_tip.string = customData == 0 ? `1 Global coin  ≈  ${ClubCache.gold_to_usdt_rate} Union coin` : `1 Union coin  ≈  ${ClubCache.usdt_to_gold_rate} Global coin`
     }
+
+
     editBoxChangeCb() {
-        if (this._selectTitle == 0) {
-            if (this.getString(this.goldEditBox.string)) {
-                this.lbl_gold.string = this.goldEditBox.string + ' 金豆'
-            } else {
-                UIComponent.Instance.Toast('最多保留两位小数')
-            }
-            this.saveBtn.interactable = this.goldEditBox.string != ''
+        this.lbl_tip.string = this.selectIndex == 0 ? `1 Global coin  ≈  ${Number(this.editBox.string)} Union coin` : `1 Union coin  ≈  ${Number(this.editBox.string)} Global coin`
 
-
-        } else {
-            if (this.getString(this.usdtEditBox.string)) {
-                this.lbl_usdt.string = this.usdtEditBox.string + ' USDT'
-            } else {
-                UIComponent.Instance.Toast('最多保留两位小数')
-            }
-            this.saveBtn.interactable = this.usdtEditBox.string != ''
-
-        }
     }
     getString(string: String) {
 
@@ -98,10 +74,18 @@ export default class UIClubRateSet extends BaseForm {
         return true
     }
     async saveRate() {
-        ClubCache._msg.gold_to_usdt_rate = Number(this.usdtEditBox.string)
-        ClubCache._msg.usdt_to_gold_rate = Number(this.goldEditBox.string)
+        if (this.editBox.string == '') {
+            this.editBox.string = 0 + ""
+        }
+        if (this.selectIndex == 0) {
+            ClubCache._msg.gold_to_usdt_rate = Number(this.editBox.string)
+
+        } else {
+            ClubCache._msg.usdt_to_gold_rate = Number(this.editBox.string)
+
+        }
         await UIClubModel.mInstance.APIOrgChangeClubData({ club_id: ClubCache.club_id, usdt_to_gold_rate: ClubCache.usdt_to_gold_rate, gold_to_usdt_rate: ClubCache.gold_to_usdt_rate })
-        this.close();
+        ToastManager.Instance.createToast(i18nMgr.Get("error0"));
     }
 
 }
