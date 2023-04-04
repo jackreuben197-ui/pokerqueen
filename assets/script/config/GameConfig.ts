d6d40034a4ffa92f47d82a539e70d562e91befe8
yechun <21882889@qq.com>
2023-04-04 13:03:06 +0800

    fix

--- assets/script/config/GameConfig.ts
+++ assets/script/config/GameConfig.ts
@@ -29,7 +29,7 @@ export class GameConfig {
     //4: https://dev1.awanptest.com 
     static BuildType: number = 5;
     //版本号
-    static Version: string = "20230330_1400";
+    static Version: string = "20230404_1300";
 
     static Default_Language: string = "cn";
     //是否使用代理
