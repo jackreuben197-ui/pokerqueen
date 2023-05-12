088a86e44f61f674fa9f42d7a3bbda122d0f8c44
yechun <21882889@qq.com>
2023-05-12 12:07:56 +0800

    fix

--- assets/script/manager/SceneManager.ts
+++ assets/script/manager/SceneManager.ts
@@ -70,7 +70,8 @@ export default class SceneManager {
             this.currUI.getComponent(BaseScene)?.Exit(currExitParams);
         }
         if (newUI) {
-            newUI.active = true;
+            //newUI.active = true;
+            newUI.opacity = 255;
             newUI.parent = this.UILayer;
             newUI.getComponent(BaseScene)?.Enter(newEnterParams);
             this.currUI = newUI;
