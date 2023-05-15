65032e49f1fc367c343226a799230900b1f9bdaf
yechun <21882889@qq.com>
2023-05-16 01:21:19 +0800

    fix

--- assets/script/config/GameConfig.ts
+++ assets/script/config/GameConfig.ts
@@ -14,7 +14,7 @@ import { INetWork } from "../define/EIDefine";
 
 export class GameConfig {
 
-    static publish: boolean = true;
+    static publish: boolean = false;
 
     //需要清理storage,就递增这个值
     static clean_all_flag: number = 2;
