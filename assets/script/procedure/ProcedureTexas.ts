340e90e97481ef15ecaf2d3905d8bfda5b340e1c
guysoup027 <guysoup027@gmail.com>
2026-05-02 10:10:56 +0800

    update: remove unncessary procedure

--- assets/script/procedure/ProcedureTexas.ts
+++ assets/script/procedure/ProcedureTexas.ts
@@ -5,7 +5,6 @@ import { RoomType } from "../game/util/GameUtil";
 import { TexasGameState } from "../game/TexasGameState";
 
 import ProcedureBase from "./ProcedureBase";
-import ProcedureConfig from "./ProcedureConfig";
 import GC from "../frame/GameControl";
 import H5MsgMgr from "../H5MsgMgr";
 import UIComponent from "../ui/UIComponent";
@@ -22,9 +21,6 @@ export default class ProcedureTexas extends ProcedureBase {
     lateEnter(param?: any) {
         super.lateEnter(param);
         // H5 桥接模式下可能跳过了 ProcedureConfig，确保 Network 已初始化
-        if (!GameConfig.Network) {
-            ProcedureConfig.setNetwork();
-        }
         GameCache.Instance.InitTexasGame();
         GameCache.Instance.CurGame.Enter();
         GameCache.Instance.CurGame.SMAgency.ChangeGameState(TexasGameState.Launch);
