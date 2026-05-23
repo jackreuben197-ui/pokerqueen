7082f6777b423b47cf9d7d59baccc29296b78a8c
guysoup027 <guysoup027@gmail.com>
2026-05-23 22:03:20 +0800

    fix: reduce is not same as await

--- assets/script/ui/UIPreloadingComponent.ts
+++ assets/script/ui/UIPreloadingComponent.ts
@@ -38,21 +38,20 @@ export default class UIPreloadingComponent extends UIBase {
         this.progress_desc.string = content;
     }
 
-    onShow(param?: PreloadParams) {
+    async onShow(param?: PreloadParams) {
         super.onShow(param);
         this.setProgress(0);
         const parts = param.preloadDefinition.length;
-        let part = Math.round(10000 / parts / 10000);
-        param.preloadDefinition.reduce(
-            (chain, definition, index) => {
-                return chain.then(() => {
-                    this.loadResources(definition.bundle, definition.dir, param.stopProgress, index * part);
-                });
-            },
-            Promise.resolve().then(() => {
-                param.complete?.();
-            })
-        );
+        let part = Math.round(10000 / parts )/ 10000;
+        try {
+            for (let i= 0; i< parts; i++) {
+                const definition = param.preloadDefinition[i];
+                await this.loadResources(definition.bundle, definition.dir, param.stopProgress, i* part);
+            }
+            param.complete?.();
+        } catch(e) {
+            param.error?.(e instanceof Error ? e : new Error(String(e)));
+        }
     }
 
     private loadResources(bundleName: string, dir: string, stopProgress: boolean, pastProgress: number): Promise<void> {
