e59b6f279ffd2b0809f87636a8c5d58c0c6a5d65
osoker <osoker008@gmail.com>
2026-06-02 13:03:14 +0800

    加入桌布替换的逻辑框架

--- assets/script/game/texas/TexasGame.ts
+++ assets/script/game/texas/TexasGame.ts
@@ -639,9 +639,23 @@ export default class TexasGame {
         return this.setting.deskType;
     }
 
+    /** 桌布贴图名称映射：索引对应 deskType，值为 texture_table prefab 中的节点名 */
+    private static readonly DESK_TEXTURE_MAP: string[] = [
+        'new_ui_top_table_0', // 0 - 默认桌布
+        'desk1',              // 1
+        'desk2',              // 2
+        'desk3',              // 3
+        'desk4',              // 4
+        'desk5',              // 5
+        'desk6',              // 6
+    ];
+
     SetDeskType(type: number) {
         this.setting.deskType = type;
-        this.uirc.sp_table_bg.spriteFrame = AssetContext.getAsset(`new_ui_top_table_0`, AssetFold.texture_table);
+        const textureName = TexasGame.DESK_TEXTURE_MAP[type] || TexasGame.DESK_TEXTURE_MAP[0];
+        const spriteFrame = AssetContext.getAsset(textureName, AssetFold.texture_table)
+            || AssetContext.getAsset(TexasGame.DESK_TEXTURE_MAP[0], AssetFold.texture_table);
+        this.uirc.sp_table_bg.spriteFrame = spriteFrame;
         if (this.isBombPot) {
             this.bombPotFeature?.PlayOpenScreen();
         }
